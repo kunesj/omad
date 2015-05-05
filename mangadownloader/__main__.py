3bb61568a8ddc4e377a0195ad939e0b2f93e36bf
Jiří Kuneš <jirka642@gmail.com>
2015-05-05 16:07:16 +0200

    fixed __main__.py header

--- mangadownloader/__main__.py
+++ mangadownloader/__main__.py
@@ -1,20 +1,20 @@
-# ! /usr/bin/python
+#!/usr/bin/python2
 # coding: utf-8
 """
-This file is part of EH File Manager.
+This file is part of Manga Downloader.
 
-EH File Manager is free software: you can redistribute it and/or modify
+Manga Downloader is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
+the Free Software Foundation, either version 3 of the License, or
 any later version.
 
-EH File Manager is distributed in the hope that it will be useful,
+Manga Downloader is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with EH File Manager.  If not, see <http://www.gnu.org/licenses/>.
+along with Manga Downloader.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from mangadownloader_app import main

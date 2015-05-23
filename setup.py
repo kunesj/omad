356dd24a5e145044fa8bc65860e43b6fd0c65036
Jiří Kuneš <jirka642@gmail.com>
2015-05-23 15:22:09 +0200

    update version with __init__.py

--- setup.py
+++ setup.py
@@ -3,21 +3,25 @@
 
 from setuptools import setup
 
+import omad
+from os.path import *
+
 setup(name='omad',
-        version='0.2-pre2',
-        description='Online MAnga Downloader, simple application for downloading chapters from online manga reading sites.',
-        author='Jiří Kuneš',
-        author_email='jirka642@gmail.com',
-        url='https://github.com/kunesj/omad',
-        download_url = 'https://github.com/kunesj/omad/tarball/v0.2-pre2',
+        version = omad.__version__,
+        description = 'Online MAnga Downloader, simple application for downloading chapters from online manga reading sites.',
+        long_description = open(join(dirname(__file__), 'README.md')).read(),
+        author = 'Jiří Kuneš',
+        author_email = 'jirka642@gmail.com',
+        url = 'https://github.com/kunesj/omad',
+        download_url = 'https://github.com/kunesj/omad/tarball/v'+str(omad.__version__),
         keywords = ['manga', 'download'],
-        packages=['omad'],
-        include_package_data=True,
-        license="GPL3",
-        entry_points={
+        packages = ['omad'],
+        include_package_data = True,
+        license = "GPL3",
+        entry_points = {
         'console_scripts': ['omad = omad.omad_app:main'],
         },
-        install_requires=[
+        install_requires = [
           'setuptools',
           'beautifulsoup',
           'requests'

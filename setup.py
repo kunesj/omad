9438791198e676fe1f1326a371a151c3019d9340
Jiří Kuneš <jirka642@gmail.com>
2015-05-23 14:34:14 +0200

    ready for pip

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@
 from setuptools import setup
 
 setup(name='omad',
-        version='0.1',
+        version='0.2-pre2',
         description='Online MAnga Downloader, simple application for downloading chapters from online manga reading sites.',
-        long_description=open("README.md").read(),
         author='Jiří Kuneš',
         author_email='jirka642@gmail.com',
         url='https://github.com/kunesj/omad',
+        download_url = 'https://github.com/kunesj/omad/tarball/v0.2-pre2',
+        keywords = ['manga', 'download'],
         packages=['omad'],
         include_package_data=True,
         license="GPL3",
@@ -17,6 +18,7 @@ setup(name='omad',
         'console_scripts': ['omad = omad.omad_app:main'],
         },
         install_requires=[
+          'setuptools',
           'beautifulsoup',
           'requests'
           # python-qt4 

d7c785cfe57fa727e80218be1ff8ae38f2340e24
Jiří Kuneš <jirka642@gmail.com>
2015-11-11 02:50:59 +0100

    trying to fix kissmanga parser

--- omad/kissmanga_model.py
+++ omad/kissmanga_model.py
@@ -30,15 +30,81 @@ except:
 
 from archive_controller import ArchiveController
 
-class KissmangaModel():
+class KissmangaModel():    
     def __init__(self, series_url, gui_info_fcn):
         # default is download_controller.defaultInfoFcn
         self.gui_info_fcn = gui_info_fcn
         self.series_url = series_url
         
-    def getChaptersList(self):
-        r = requests.get(self.series_url, timeout=30)
+        self.cookies = {}
+    
+    def getRealPage(self, html, url):
+        import js2py
+        
+        html = unicode(html)
+        soup = BeautifulSoup(html)
+        
+        hidden_form = soup.body.find('form', attrs={'id':"challenge-form"})
+        if hidden_form is None:
+            return html
+        
+        get_params = {"jschl_vc":None, "pass":None, "jschl_answer":None}
+        inputs = hidden_form.findAll('input')
+        for i in inputs:
+            get_params[i.get('name')] = i.get('value')
+        
+        # get important parts of script
+        script = soup.head.find('script').text
+        script_setTimeout_fcn = script.split("setTimeout(function(){")[-1].split("}")[0]+"}"
+        script_fcn_end = script.split("setTimeout(function(){")[-1].split("}")[1]
+        #print script_setTimeout_fcn
+        #print script_fcn_end 
+        
+        # get script lines
+        script_lines = []
+        script_lines += [script_setTimeout_fcn.split(';')[0].strip()]
+        for l in script_fcn_end.split('\n'):
+            l_split = l.split(';')
+            #filter and modify lines
+            l_filtered = []
+            for ls in l_split:
+                ls = ls.strip()
+                if ls.startswith('a.value ='):
+                    ls = ls.replace('a.value =', 'a =')
+                elif ls.startswith("t = t.firstChild.href"):
+                    ls = "t = 'http://kissmanga.com/'"
+                
+                if ls == '' or ls.startswith('f.submit') or ls.startswith('f = ') or ls.startswith('a = document.get')\
+                    or ls.startswith('t = document.') or ls.startswith('t.innerHTML'):
+                    continue
+                else:
+                    l_filtered.append(ls)
+            script_lines += l_filtered
+        script_edited = ";\n".join(script_lines)+";"
+        #print script_edited
+        
+        result = js2py.eval_js('function fcn() {\n'+script_edited+'\nreturn a;};')()
+        get_params["jschl_answer"] = result
+        #print result
+        
+        custom_headers = {'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0',
+                    'referer': url, 'host':"kissmanga.com"}
+        
+        r = requests.get("http://kissmanga.com/cdn-cgi/l/chk_jschl", timeout=30, params=get_params, headers=custom_headers, cookies=self.cookies)
+        self.cookies.update(r.cookies)
         html = unicode(r.text)
+        
+        #print html
+        #print get_params
+        
+        print "Not finished!!!"
+        exit(1) # TODO
+        
+    def getChaptersList(self):
+        r = requests.get(self.series_url, timeout=30, cookies=self.cookies)
+        self.cookies.update(r.cookies)
+        html = unicode(r.text)
+        #html = self.getRealPage(html, self.series_url)
         soup = BeautifulSoup(html)
 
         table_ch = soup.body.find('table', attrs={'class':'listing'})
@@ -64,8 +130,10 @@ class KissmangaModel():
         full_gallery_url = chapter[1]
         
         try:
-            r = requests.get(full_gallery_url, timeout=30)
+            r = requests.get(full_gallery_url, timeout=30, cookies=self.cookies)
+            self.cookies.update(r.cookies)
             html = unicode(r.text)
+            #html = self.getRealPage(html, full_gallery_url)
             soup = BeautifulSoup(html)
             
             # parse html

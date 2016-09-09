c3b0543dd88625075ed1aff3100a69de7db2c6ae
Jiří Kuneš <jirka642@gmail.com>
2016-09-09 14:12:01 +0200

    fixed setup.py subpackage problem

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
+import os
 from setuptools import setup
 
 import omad
-from os.path import *
 
 setup(name='omad',
         version = omad.__version__,
         description = 'Online MAnga Downloader, simple application for downloading chapters from online manga reading sites.',
-        long_description = open(join(dirname(__file__), 'README.md')).read(),
+        long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
         author = 'Jiří Kuneš',
         author_email = 'jirka642@gmail.com',
         url = 'https://github.com/kunesj/omad',
         download_url = 'https://github.com/kunesj/omad/tarball/v'+str(omad.__version__),
         keywords = ['manga', 'download'],
-        packages = ['omad'],
+        packages = ['omad', 'omad.misctools'],
         include_package_data = True,
         license = "GPL3",
         entry_points = {
